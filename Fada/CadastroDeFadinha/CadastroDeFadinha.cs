78fc4a111709ce9eb0e4ecacd9d6758cda21b848
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-18 15:26:43 -0300

    Exercício de fada inicializado;
    Conclusão de edição de cadastro normal;

---
+++ Fada/CadastroDeFadinha/CadastroDeFadinha.cs
@@ -0,0 +1,154 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroDeFadinha
+{
+    public partial class CadastroDeFadinha : Form
+    {
+        string nomeAntigo = "";
+        bool novo = true;
+        List<Fadinhas> fadinha = new List<Fadinhas>();
+
+        public CadastroDeFadinha()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 2;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 1;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 0;
+        }
+
+        private void btnSalvarFadinha_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Fadinhas fadinhas = new Fadinhas();
+
+                {
+
+
+                    fadinhas.Nome = txtNomeFadinha.Text;
+                    fadinhas.Familia = txtFamiliaFadinha.Text;
+                    fadinhas.Cor = txtCorFadinha.Text;
+                    fadinhas.CorAsa = txtCorAsa.Text;
+                    fadinhas.TamanhoAsa = Convert.ToDouble(txtTamanhoAsa.Text);
+                    fadinhas.Elemento = txtElemento.Text;
+                };
+
+                if (nomeAntigo == "")
+                {
+                    fadinha.Add(fadinhas);
+                    MessageBox.Show("Cadastrado com sucesso");
+                    AdicionarFadaATabela(fadinhas);
+                }
+                else
+                {
+                    int linha = fadinha.FindIndex(x => x.Nome == nomeAntigo);
+                    fadinha[linha] = fadinhas;
+                    EditarFadinhaNaTabela(fadinhas, linha);
+                    MessageBox.Show("Alterado com sucesso");
+                    nomeAntigo = string.Empty;
+                }
+                LimparCampos();
+                tabControl1.SelectedIndex = 0;
+            }
+
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+        }
+
+        private void LimparCampos()
+        {
+           
+            txtNomeFadinha.Text = "";
+            txtFamiliaFadinha.Text = "";
+            txtCorFadinha.Text = "";
+            txtCorAsa.Text = "";
+            txtTamanhoAsa.Text = "";
+            txtElemento.Text = "";
+
+        }
+
+        private void EditarFadinhaNaTabela(Fadinhas fadinhas, int linha)
+        {
+            dataGridView1.Rows[linha].Cells[0].Value = fadinhas.Nome;
+            dataGridView1.Rows[linha].Cells[1].Value = fadinhas.Familia;
+            dataGridView1.Rows[linha].Cells[2].Value = fadinhas.Cor;
+            dataGridView1.Rows[linha].Cells[3].Value = fadinhas.CorAsa;
+            dataGridView1.Rows[linha].Cells[4].Value = fadinhas.TamanhoAsa;
+            dataGridView1.Rows[linha].Cells[5].Value = fadinhas.Elemento;
+        }
+
+        private void AdicionarFadaATabela(Fadinhas fadinhas)
+        {
+            dataGridView1.Rows.Add(new Object[]{
+                fadinhas.Nome, fadinhas.Familia, fadinhas.Cor, fadinhas.CorAsa, fadinhas.TamanhoAsa, fadinhas.Elemento
+            });
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Cadastre uma fadinha!");
+                tabControl1.SelectedIndex = 2;
+                return;
+            }
+            if (dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("Selecione um arquivo para editar");
+                return;
+            }
+            int linhaSelecionada = dataGridView1.CurrentRow.Index;
+            string nome = dataGridView1.Rows[linhaSelecionada].Cells[0].Value.ToString();
+            foreach (Fadinhas fadinhas in fadinha)
+            {
+                if (fadinhas.Nome == nome)
+                {
+                    txtNomeFadinha.Text = fadinhas.Nome;
+                    txtFamiliaFadinha.Text = fadinhas.Familia;
+                    txtCorFadinha.Text = fadinhas.Cor;
+                    txtCorAsa.Text = fadinhas.CorAsa;
+                    txtTamanhoAsa.Text = Convert.ToString(fadinhas.TamanhoAsa);
+                    txtElemento.Text = fadinhas.Elemento;
+                    nomeAntigo = fadinhas.Nome;
+                    tabControl1.SelectedIndex = 2;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Fada/Model/Fadinhas.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Fadinhas
+    {
+        private string nome;
+        private string familia;
+        private string cor;
+        private string corAsa;
+        private double tamanhoAsa;
+        private bool asaQuebrada;
+        private bool ehMulher;
+        private bool fazBarulho;
+        private string elemento;
+
+         public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+            set
+            {
+                
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Nome deve ser preenchido");
+                }
+
+                if (value.Count() < 3)
+                {
+                    throw new Exception("Nome da fada deve conter no mínimo 3 caracteres");
+                }
+
+                if (value.Count() > 30)
+                {
+                    throw new Exception("Nome da fada deve conter no máximo 30 caracteres");
+                }
+                nome = value;
+            }
+         }
+
+
+         public string Familia
+         {
+             get
+             {
+                 return familia;
+             }
+             set
+             {
+
+                 if (string.IsNullOrEmpty(value))
+                 {
+                     throw new Exception("Familia deve ser preenchida");
+                 }
+
+                 if (value.Count() < 3)
+                 {
+                     throw new Exception("Nome da família deve conter no mínimo 3 caracteres");
+                 }
+
+                 if (value.Count() > 30)
+                 {
+                     throw new Exception("Nome da família deve conter no máximo 30 caracteres");
+                 }
+                 familia = value;
+             }
+         }
+
+         public string Cor
+         {
+             get
+             {
+                 return cor;
+             }
+             set
+             {
+
+                 if (string.IsNullOrEmpty(value))
+                 {
+                     throw new Exception("Cor deve ser preenchida");
+                 }
+
+                 if (value.Count() < 3)
+                 {
+                     throw new Exception("Cor deve conter no mínimo 4 caracteres");
+                 }
+
+                 if (value.Count() > 20)
+                 {
+                     throw new Exception("Cor deve conter no máximo 20 caracteres");
+                 }
+                 cor = value;
+             }
+         }
+
+
+
+         public string CorAsa
+         {
+             get
+             {
+                 return corAsa;
+             }
+             set
+             {
+
+                 if (string.IsNullOrEmpty(value))
+                 {
+                     throw new Exception("Cor da asa deve ser preenchida");
+                 }
+
+                 if (value.Count() < 3)
+                 {
+                     throw new Exception("Cor da asa deve conter no mínimo 4 caracteres");
+                 }
+
+                 if (value.Count() > 20)
+                 {
+                     throw new Exception("Cor da asa deve conter no máximo 20 caracteres");
+                 }
+                 corAsa = value;
+             }
+         }
+
+
+
+         public double TamanhoAsa
+         {
+             get
+             {
+                 return tamanhoAsa;
+             }
+             set
+             {
+
+                 if (value <= 0)
+                 {
+                     throw new Exception("Tamanho deve ser preenchido");
+                 }
+
+                 if (value < 3)
+                 {
+                     throw new Exception("Tamanho deve conter no mínimo 4 caracteres");
+                 }
+
+                 if (value > 20)
+                 {
+                     throw new Exception("Tamanho deve conter no máximo 20 caracteres");
+                 }
+                 tamanhoAsa = value;
+             }
+         }
+
+
+         public string Elemento
+         {
+             get
+             {
+                 return elemento;
+             }
+             set
+             {
+
+                 if (string.IsNullOrEmpty(value))
+                 {
+                     throw new Exception("Elemento deve ser preenchido");
+                 }
+
+                 if (value.Count() < 3)
+                 {
+                     throw new Exception("Elemento deve conter no mínimo 4 caracteres");
+                 }
+
+                 if (value.Count() > 20)
+                 {
+                     throw new Exception("Elemento deve conter no máximo 20 caracteres");
+                 }
+                 elemento = value;
+             }
+         }
+
+
+
+        
+
+        
+
+    }
+}
